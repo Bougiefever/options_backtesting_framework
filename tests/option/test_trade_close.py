6d8a55f954085eca27ff2bf6bf4c00557b8df4d1
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-09 22:01:47 -0700

    relocate open and close tests, change file names to unified namespace

---
+++ tests/option/test_trade_close.py
@@ -0,0 +1,18 @@
+
+def test_trade_close_records_returns_all_close_trades():
+    test_option = get_test_call_option()
+    test_option.open_trade(10)
+    quote_date, spot_price, bid, ask, price = get_test_call_option_update_values_1()
+    test_option.update(quote_date, spot_price, bid, ask, price)
+
+    test_option.close_trade(quantity=3)
+
+    records = test_option.trade_close_records
+    assert len(records) == 1
+
+    quote_date, spot_price, bid, ask, price = get_test_call_option_update_values_2()
+    test_option.update(quote_date, spot_price, bid, ask, price)
+    test_option.close_trade(quantity=6)
+
+    records = test_option.trade_close_records
+    assert len(records) == 2
