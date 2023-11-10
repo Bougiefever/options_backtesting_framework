e754efd8fd18ff1107fbf979a2a87db53c14306c
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-10 00:14:58 -0700

    refactor test to use conftest.py fixtures

--- tests/option/test_get_profit_loss_percent.py
+++ tests/option/test_get_profit_loss_percent.py
@@ -1,30 +1,44 @@
+import pytest
+from options_framework.config import settings
 
 
-def test_get_profit_loss_percent_return_zero_when_no_contracts_are_open():
-    test_option = get_test_call_option()
+def test_get_profit_loss_percent_return_zero_when_no_contracts_are_open(call_option):
+    test_option = call_option
     test_option.open_trade(1)
     test_option.close_trade(1)
 
     assert test_option.get_open_profit_loss_percent() == 0.0
 
 
-@pytest.mark.parametrize("test_option, quantity, price, expected_profit_loss_pct", [
-    (get_test_call_option(), 10, 2.25, 0.5),
-    (get_test_call_option(), 10, 1.17, -0.22),
-    (get_test_call_option(), 10, 1.5, 0.0),
-    (get_test_call_option(), -10, 2.25, -0.50),
-    (get_test_call_option(), -10, 1.17, 0.22),
-    (get_test_call_option(), -10, 1.5, 0.0),
-    (get_test_put_option(), 10, 2.25, 0.5),
-    (get_test_put_option(), 10, 1.17, -0.22),
-    (get_test_put_option(), 10, 1.5, 0.0),
-    (get_test_put_option(), -10, 2.25, -0.50),
-    (get_test_put_option(), -10, 1.17, 0.22),
-    (get_test_put_option(), -10, 1.5, 0.0)
-])
-def test_get_profit_loss_percent_value(test_option, quantity, price, expected_profit_loss_pct):
+@pytest.mark.parametrize(
+    "option_type, quantity, price, expected_profit_loss_pct",
+    [
+        (settings.OptionType.CALL, 10, 2.25, 0.5),
+        (settings.OptionType.CALL, 10, 1.17, -0.22),
+        (settings.OptionType.CALL, 10, 1.5, 0.0),
+        (settings.OptionType.CALL, -10, 2.25, -0.50),
+        (settings.OptionType.CALL, -10, 1.17, 0.22),
+        (settings.OptionType.CALL, -10, 1.5, 0.0),
+        (settings.OptionType.PUT, 10, 2.25, 0.5),
+        (settings.OptionType.PUT, 10, 1.17, -0.22),
+        (settings.OptionType.PUT, 10, 1.5, 0.0),
+        (settings.OptionType.PUT, -10, 2.25, -0.50),
+        (settings.OptionType.PUT, -10, 1.17, 0.22),
+        (settings.OptionType.PUT, -10, 1.5, 0.0),
+    ],
+)
+def test_get_profit_loss_percent_value(
+    call_option,
+    put_option,
+    call_option_update_values_1,
+    option_type,
+    quantity,
+    price,
+    expected_profit_loss_pct,
+):
+    test_option = call_option if option_type == settings.OptionType.CALL else put_option
     test_option.open_trade(quantity)
-    quote_date, spot_price, bid, ask, _ = get_test_call_option_update_values_1()
+    quote_date, spot_price, bid, ask, _ = call_option_update_values_1
     test_option.update(quote_date, spot_price, bid, ask, price)
 
     actual_profit_loss = test_option.get_open_profit_loss_percent()
