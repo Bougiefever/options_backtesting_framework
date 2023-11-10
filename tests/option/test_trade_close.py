f7ed2e462ec86a295d34db06813ad86f2abe0003
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-09 23:01:25 -0700

    refactored tests for trade close to work with conftest fixtures

--- tests/option/test_trade_close.py
+++ tests/option/test_trade_close.py
@@ -1,8 +1,20 @@
+"""
+This module contains tests for the trade_close_records property of the Option class.
+"""
 
-def test_trade_close_records_returns_all_close_trades():
-    test_option = get_test_call_option()
+
+def test_trade_close_records_returns_all_close_trades(call_option,call_option_update_values_1,call_option_update_values_2):
+    """
+    Test that the trade_close_records property returns all close trades.
+    
+    Parameters:
+    call_option (Option): An instance of the Option class.
+    option_update_values_1 (tuple): A tuple containing quote_date, spot_price, bid, ask, price values for the first update.
+    option_update_values_2 (tuple): A tuple containing quote_date, spot_price, bid, ask, price values for the second update.
+    """
+    test_option = call_option
     test_option.open_trade(10)
-    quote_date, spot_price, bid, ask, price = get_test_call_option_update_values_1()
+    quote_date, spot_price, bid, ask, price = call_option_update_values_1
     test_option.update(quote_date, spot_price, bid, ask, price)
 
     test_option.close_trade(quantity=3)
@@ -10,7 +22,7 @@ def test_trade_close_records_returns_all_close_trades():
     records = test_option.trade_close_records
     assert len(records) == 1
 
-    quote_date, spot_price, bid, ask, price = get_test_call_option_update_values_2()
+    quote_date, spot_price, bid, ask, price = call_option_update_values_2
     test_option.update(quote_date, spot_price, bid, ask, price)
     test_option.close_trade(quantity=6)
 
