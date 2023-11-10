3db3b7c16741e11de7367bf919f330782f72be11
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-09 21:18:59 -0700

    black format

--- src/options_framework/cli.py
+++ src/options_framework/cli.py
@@ -6,25 +6,30 @@ def main():
     """Main command group."""
     pass
 
+
 @main.group()
 def config():
     """Configuration command group."""
     pass
+
 
 @config.command()
 def show():
     """Show configuration command."""
     click.echo("Showing configuration...")
 
+
 @main.group()
 def backtest():
     """Backtest command group."""
     pass
 
+
 @backtest.command()
 def butterfly():
     """Butterfly backtest command."""
     click.echo("Running butterfly backtest...")
+
 
 @backtest.command()
 def vertical():
