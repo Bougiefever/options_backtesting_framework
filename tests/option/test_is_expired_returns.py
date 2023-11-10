fac22d2ca7cea629d789365990a70b6134985a6d
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-10 00:03:37 -0700

    black format

--- tests/option/test_is_expired_returns.py
+++ tests/option/test_is_expired_returns.py
@@ -4,49 +4,40 @@ from options_framework import option
 from options_framework.option import Option
 from options_framework.config import settings
 
-def test_is_expired_returns_none_when_no_quote_data(ticker,expiration):
+
+def test_is_expired_returns_none_when_no_quote_data(ticker, expiration):
     test_option = Option(1, ticker, 100, expiration, settings.OptionType.CALL)
 
     assert test_option.is_expired() is None
 
 
-@pytest.mark.xfail(raises=(TypeError,ValueError),reason="raises value or type error")
+@pytest.mark.xfail(raises=(TypeError, ValueError), reason="raises value or type error")
 @pytest.mark.parametrize(
     "expiration_date_test, quote_date, expected_result",
     [
         (
             datetime.strptime("07-16-2021", "%m-%d-%Y"),
-            datetime.strptime(
-                "2021-07-01 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"
-            ),
+            datetime.strptime("2021-07-01 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"),
             False,
         ),
         (
             datetime.strptime("06-30-2021", "%m-%d-%Y"),
-            datetime.strptime(
-                "2021-07-01 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"
-            ),
+            datetime.strptime("2021-07-01 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"),
             True,
         ),
         (
             datetime.strptime("07-16-2021", "%m-%d-%Y"),
-            datetime.strptime(
-                "2021-07-16 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"
-            ),
+            datetime.strptime("2021-07-16 09:45:00.000000", "%Y-%m-%d %H:%M:%S.%f"),
             False,
         ),
         (
             datetime.strptime("07-16-2021", "%m-%d-%Y"),
-            datetime.strptime(
-                "2021-07-16 16:14:00.000000", "%Y-%m-%d %H:%M:%S.%f"
-            ),
+            datetime.strptime("2021-07-16 16:14:00.000000", "%Y-%m-%d %H:%M:%S.%f"),
             False,
         ),
         (
             datetime.strptime("07-16-2021", "%m-%d-%Y"),
-            datetime.strptime(
-                "2021-07-16 16:15:00.000000", "%Y-%m-%d %H:%M:%S.%f"
-            ),
+            datetime.strptime("2021-07-16 16:15:00.000000", "%Y-%m-%d %H:%M:%S.%f"),
             True,
         ),
         (
@@ -61,7 +52,9 @@ def test_is_expired_returns_none_when_no_quote_data(ticker,expiration):
         ),
     ],
 )
-def test_is_expired_returns_correct_result(call_option,ticker, put_option,    expiration_date_test, quote_date, expected_result):
+def test_is_expired_returns_correct_result(
+    call_option, ticker, put_option, expiration_date_test, quote_date, expected_result
+):
     test_option = call_option
 
     # cheat to set data manually
