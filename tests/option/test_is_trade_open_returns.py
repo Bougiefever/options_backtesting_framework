eb041160d4aa2d9bc868fdb992439278046d8d83
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-09 23:50:39 -0700

    refactor is trade open tests to use conftest.py fixtures

--- tests/option/test_is_trade_open_returns.py
+++ tests/option/test_is_trade_open_returns.py
@@ -1,21 +1,35 @@
+"""
+This module contains tests for the is_trade_open method of the Option class.
+"""
 
 
-def test_is_trade_open_returns_false_if_no_trade_was_opened():
-    test_option = get_test_call_option()
+def test_is_trade_open_returns_false_if_no_trade_was_opened(call_option):
+    """
+    Test if the is_trade_open method returns False when no trade was opened.
+    """
+    test_option = call_option
     expected_result = False
     assert test_option.is_trade_open() == expected_result
 
 
-def test_is_trade_open_returns_true_if_trade_was_opened():
-    test_option = get_test_call_option()
+def test_is_trade_open_returns_true_if_trade_was_opened(call_option):
+    """
+    Test if the is_trade_open method returns True when a trade was opened.
+    """
+    test_option = call_option
     test_option.open_trade(10)
     expected_result = True
 
     assert test_option.is_trade_open() == expected_result
 
 
-def test_is_trade_open_returns_true_if_trade_was_opened_and_partially_closed():
-    test_option = get_test_call_option()
+def test_is_trade_open_returns_true_if_trade_was_opened_and_partially_closed(
+    call_option,
+):
+    """
+    Test if the is_trade_open method returns True when a trade was opened and partially closed.
+    """
+    test_option = call_option
     test_option.open_trade(10)
     test_option.close_trade(5)
     expected_result = True
@@ -23,11 +37,13 @@ def test_is_trade_open_returns_true_if_trade_was_opened_and_partially_closed():
     assert test_option.is_trade_open() == expected_result
 
 
-def test_is_trade_open_returns_false_if_trade_was_opened_and_then_closed():
-    test_option = get_test_call_option()
+def test_is_trade_open_returns_false_if_trade_was_opened_and_then_closed(call_option):
+    """
+    Test if the is_trade_open method returns False when a trade was opened and then fully closed.
+    """
+    test_option = call_option
     test_option.open_trade(10)
     test_option.close_trade(10)
     expected_result = False
 
     assert test_option.is_trade_open() == expected_result
-
